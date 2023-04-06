6e9e96d5713f8c8951a6999701babdcda1c45277
assuy <43821373+assuy@users.noreply.github.com>
2023-04-06 18:00:01 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const numberBtns = document.querySelectorAll('#numBtn');
+const operationBtns = document.querySelectorAll('#opBtn');
+const clearBtn = document.getElementById('clearBtn');
+const clearAllBtn = document.getElementById('clearAllBtn');
+const deleteBtn = document.getElementById('deleteBtn');
+const dotBtn = document.getElementById('dotBtn');
+const totalBtn = document.getElementById('totalBtn');
+const negateBtn = document.getElementById('negateBtn');
+
+let number1, number2, result;
